0b3b03d0fb925db3ce5a75501795cf27c47f15b4
久保翔馬 <kuboshoma@LAB-N3077R.local>
2026-07-10 15:42:20 +0900

    Feature: Renamed package and file.

--- src/main/java/com/rabbitaats/alpha_experience/AlphaExperienceMod.java
+++ src/main/java/com/rabbitaats/alpha_experience/AlphaExperienceMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.rabbitaats.alpha_experience;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -29,8 +29,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(AlphaExperienceMod.MODID)
+public class AlphaExperienceMod
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -60,7 +60,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod(FMLJavaModLoadingContext context)
+    public AlphaExperienceMod(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
